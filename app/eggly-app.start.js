659f472c8f44446ce2343bb5b6a8c0a95e53b640
MoyArt Maximus <moyart@gmail.com>
2019-03-23 15:35:34 -0600

    Feature(HTML, JS): Added dinamic Active Class and isCurrentCategory Method

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -25,5 +25,9 @@ angular
         console.log(category);
         $scope.currentCategory = category;
     }
+    function isCurrentCategory(category){
+        return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+    }
     $scope.setCurrentCategory = setCurrentCategory;
-})
+    $scope.isCurrentCategory = isCurrentCategory;
+});
